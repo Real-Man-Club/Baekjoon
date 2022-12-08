4c6623118ed65c257e8430cd4ac61acfe84d1f72
Mootata <dbslt6872@gmail.com>
2022-12-09 00:01:34 +0900

    Mootata/15988

--- Mootata/32회차/15988.1, 2, 3 더학 3.py
+++ Mootata/32회차/15988.1, 2, 3 더학 3.py
@@ -7,6 +7,6 @@ dp = [1, 2, 4, 7]
 for t in range(int(input())):
     n = int(input())
     for i in range(len(dp), n):
-        dp.append(dp[-3] + dp[-2] + dp[-1] % 1000000009)
+        dp.append((dp[-3] + dp[-2] + dp[-1]) % 1000000009)
     
     print(dp[n - 1])
